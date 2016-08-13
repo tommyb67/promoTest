f4b5f8b2d437cc4487192d28e4b251d1f1d1ed57
Tom Brennan <tommybrennan@gmail.com>
2016-08-13 11:45:21 -0400

    added the home and about pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def hope
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/hope'
+
+  get 'static_pages/about'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  get 'static_pages/hope'
+
+  get 'static_pages/about'
+
   resources :users
   root 'users#index'
 end

88ea031a2e7bdcac305f5e54da52cc93e5eff855
Tom Brennan <tommybrennan@gmail.com>
2016-08-13 13:35:31 -0400

    close to finishing the static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+    root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/help'
 
-  resources :users
-  root 'static_pages#home'
 end
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get FILL_IN
+    assert_response FILL_IN
+  end
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Widgets Company"
   end
 
   test "should get about" do

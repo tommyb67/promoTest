f76b077c3d70761d11d5a1a75d3143f83815105b
Tom Brennan <tommybrennan@gmail.com>
2016-08-13 13:05:24 -0400

    added the help page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
+
 end

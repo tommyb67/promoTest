5a2b19d4a4e036f2a45824f7742450706584f729
Tom Brennan <tommybrennan@gmail.com>
2016-08-13 17:28:03 -0400

    added navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :users
     root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
 end
 

2490bfdcb1f4fb9e4e5d4fd8b740c282a9a33e34
Tom Brennan <tommybrennan@gmail.com>
2016-08-13 11:41:45 -0400

    resdy to set up static home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  root 'application#hello'
+  resources :users
+  root 'users#index'
 end
 
 end
